fb5e25d02762363078283d1e69080b9627455c4f
Wojciech Kumoń <wojciechkumon@gmail.com>
2024-03-16 23:39:50 +0100

    DottedLayout story added

---
+++ frontend/src/common/DottedLayout.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DottedLayout } from './DottedLayout.tsx';
+
+const meta = {
+  title: 'common/DottedLayout',
+  component: DottedLayout,
+  decorators: [
+    (Story) => (
+      <div className='bg-stone-800 p-6'>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof DottedLayout>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: <></>,
+  },
+};
